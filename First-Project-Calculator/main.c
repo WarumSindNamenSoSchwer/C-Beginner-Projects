8dc7ce83ef7253a420431200dfdbfab474129d92
WarumSindNamenSoSchwer <muri5988@gmail.com>
2024-05-19 22:57:56 +0200

    first project While loop not breaking properly

---
+++ First-Project-Calculator/main.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+
+float addition(float x, float y)
+{
+    float sum = x + y;
+    return sum;
+}
+
+float subtraction(float x, float y)
+{
+    float diff = x - y;
+    return diff;
+}
+
+float multiplication(float x, float y)
+{
+    float product = x * y;
+    return product;
+}
+
+float division(float x, float y)
+{
+    float result = x / y;
+    return result;
+}
+
+int main()
+{
+    float x, y, result;
+    char operator;
+    int True = 1;
+    
+    while (True)
+    {
+        printf("Please enter the operation you would like to peform:\n");
+        printf("+ | - | * | / | q\n");
+
+        scanf(" %c", &operator);
+        printf("You have chosen %c\n", operator);
+        
+        switch (operator)
+        {
+        case '+':
+
+            printf("Plese enter the first number for the addition: ");
+            scanf(" %f", &x);
+
+            printf("You chose %.2f Please enter the second number: ", x);
+            scanf(" %f", &y);
+
+            result = addition(x, y);
+
+            printf("The result is %.2f", result);
+
+            break;
+
+        case '-':
+
+            printf("Plese enter the first number for the subtraction: ");
+            scanf(" %f", &x);
+
+            printf("You chose %.2f Please enter the second number: ", x);
+            scanf(" %f", &y);
+
+            result = subtraction(x, y);
+
+            printf("The result is %.2f", result);
+
+            break;
+
+        case '*':
+
+            printf("Plese enter the first number for the multiplication: ");
+            scanf(" %f", &x);
+
+            printf("You chose %.2f Please enter the second number: ", x);
+            scanf(" %f", &y);
+
+            result = multiplication(x, y);
+
+            printf("The result is %.2f", result);
+
+            break;
+
+        case '/':
+
+            printf("Plese enter the first number for the division: ");
+            scanf(" %f", &x);
+
+            printf("You chose %.2f Please enter the second number: ", x);
+            scanf(" %f", &y);
+
+            result = division(x, y);
+
+            printf("The result is %.2f", result);
+
+            break;
+
+        case 'q':
+            printf("test");
+            True += 1;
+            break;
+
+        default:
+            printf("Invalid input\n");
+            break;
+        }
+    }
+
+    return 0;
+}
